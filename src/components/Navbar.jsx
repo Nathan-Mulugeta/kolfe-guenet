aad92e025da984af0c244328d6fed23d2cb0b049
nathanMulugeta01 <nathanmulugetacoding@gmail.com>
2023-03-01 13:41:20 +0300

    added animation to the nav bar based on scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ export default function Navbar() {
   const [profileOpen, setProfileOpen] = useState(false);
   const profileRef = useRef(null);
   const navRef = useRef(null);
+  const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
     document.addEventListener("click", handleProfileClickOutside);
@@ -22,6 +23,21 @@ export default function Navbar() {
       document.removeEventListener("click", handleNavClickOutside);
     };
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 0) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
 
   const handleProfileClickOutside = (event) => {
     if (profileRef.current && !profileRef.current.contains(event.target)) {
@@ -46,14 +62,16 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className=" fixed top-0 left-0 right-0 bg-secondary py-4 drop-shadow-md md:bg-secondary/20"
+        className={`fixed top-0 left-0 right-0 z-50 py-4 drop-shadow-md transition duration-500 ${
+          scroll ? "bg-white shadow-md" : "bg-transparent"
+        }`}
         ref={navRef}
       >
         {/* Nav bar container */}
 
         <div className="container mx-auto flex items-center justify-between px-4">
           {/* Nav bar hamburger icon */}
-          <div className="rounded-md bg-secondary/50 px-1">
+          <div className={`${scroll ? "" : "rounded-md bg-secondary/50"} px-1`}>
             <button
               onClick={handleNavClick}
               id="menu-btn"
@@ -61,16 +79,26 @@ export default function Navbar() {
                 navOpen ? "open" : ""
               } hamburger focus:outline-none md:hidden`}
             >
-              <span className="hamburger-top"></span>
-              <span className="hamburger-middle"></span>
-              <span className="hamburger-bottom"></span>
+              <span
+                className={`${scroll ? "bg-black" : "bg-white"} hamburger-top`}
+              ></span>
+              <span
+                className={`${
+                  scroll ? "bg-black" : "bg-white"
+                } hamburger-middle`}
+              ></span>
+              <span
+                className={`${
+                  scroll ? "bg-black" : "bg-white"
+                } hamburger-bottom`}
+              ></span>
             </button>
           </div>
 
           {/* Church Logo */}
           <NavLink to="/">
             <img
-              className="h-8 w-auto rounded-md bg-white"
+              className={`h-8 w-auto rounded-md bg-white`}
               src={churchLogo}
               alt="Kolfe Guenet Church"
             />
@@ -82,8 +110,10 @@ export default function Navbar() {
               to="/"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive ? "bg-secondary text-white" : "hover:bg-secondary/25"
+                } rounded-md px-3 py-1 ${
+                  scroll ? "text-secondary" : "text-white"
+                } transition duration-200 ease-in-out`
               }
             >
               Home
@@ -92,8 +122,10 @@ export default function Navbar() {
               to="/B"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive ? "bg-secondary text-white" : "hover:bg-secondary/25"
+                } rounded-md px-3 py-1 ${
+                  scroll ? "text-secondary" : "text-white"
+                } transition duration-200 ease-in-out`
               }
             >
               Team
@@ -102,8 +134,10 @@ export default function Navbar() {
               to="/C"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive ? "bg-secondary text-white" : "hover:bg-secondary/25"
+                } ${
+                  scroll ? "text-secondary" : "text-white"
+                } rounded-md px-3 py-1  transition duration-200 ease-in-out`
               }
             >
               Calendar
@@ -112,8 +146,10 @@ export default function Navbar() {
               to="/D"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive ? "bg-secondary text-white" : "hover:bg-secondary/25"
+                } rounded-md px-3 py-1 ${
+                  scroll ? "text-secondary" : "text-white"
+                } transition duration-200 ease-in-out`
               }
             >
               Projects
@@ -127,7 +163,11 @@ export default function Navbar() {
             className="relative"
           >
             <button>
-              <RxAvatar className="h-8 w-8 text-white" />
+              <RxAvatar
+                className={`h-8 w-8 ${
+                  scroll ? "text-secondary" : "text-white"
+                } transition-all duration-500 ease-in-out `}
+              />
             </button>
             <div className={`${profileOpen ? "" : "hidden"}`}>
               <ul className="absolute right-1 flex w-36  flex-col gap-2 rounded-md bg-white px-6 py-4 font-medium text-secondary transition-all duration-1000 ease-in-out">
@@ -159,14 +199,16 @@ export default function Navbar() {
           ref={navRef}
         >
           {/* Mobile menu items */}
-          <ul className="absolute left-6 right-6 mt-10 flex flex-col items-center justify-center gap-4 bg-secondary/20 py-8 font-bold drop-shadow-md sm:w-auto">
+          <ul className="absolute left-6 right-6 mt-10 flex flex-col items-center justify-center gap-4 rounded-3xl bg-white py-8 font-bold drop-shadow-md sm:w-auto">
             <NavLink
               onClick={handleNavClick}
               to="/"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive
+                    ? "bg-secondary text-white"
+                    : "hover:text-secondary/50"
+                } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
               Home
@@ -176,8 +218,10 @@ export default function Navbar() {
               to="/B"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive
+                    ? "bg-secondary text-white"
+                    : "hover:text-secondary/50"
+                } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
               Team
@@ -187,8 +231,10 @@ export default function Navbar() {
               to="/C"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive
+                    ? "bg-secondary text-white"
+                    : "hover:text-secondary/50"
+                } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
               Calendar
@@ -198,8 +244,10 @@ export default function Navbar() {
               to="/D"
               className={({ isActive }) =>
                 `${
-                  isActive ? "bg-secondary/50 text-white" : "hover:text-primary"
-                } rounded-md px-3 py-1 text-white transition duration-200 ease-in-out`
+                  isActive
+                    ? "bg-secondary text-white"
+                    : "hover:text-secondary/50"
+                } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
               Price
