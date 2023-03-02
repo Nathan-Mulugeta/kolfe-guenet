608ba55bcc270d3e3a61b3eea625fa994b495434
nathanMulugeta01 <nathanmulugetacoding@gmail.com>
2023-03-02 15:00:59 +0300

    to see changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,25 +24,20 @@ function Hero() {
               Getting started with Kolfe Guenet church.
             </motion.p>
           </div>
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1, type: "spring", delay: 0.5 }}
+
+          <motion.button
+            whileHover={{
+              scale: 1.1,
+              boxShadow: "0 4px 3px rgb(0 0 0 / 0.07)",
+            }}
+            whileTap={{ scale: 1 }}
+            className="flex items-center gap-3 rounded-md bg-secondary/75 py-2 px-6 font-bold text-white transition-all duration-200 ease-in-out"
           >
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                boxShadow: "0 4px 3px rgb(0 0 0 / 0.07)",
-              }}
-              whileTap={{ scale: 1 }}
-              className="flex items-center gap-3 rounded-md bg-secondary/75 py-2 px-6 font-bold text-white transition-all duration-200 ease-in-out"
-            >
-              <p>Connect with us</p>
-              <span className="rounded-full border-2 py-2 px-2">
-                <FaPeopleArrows className="text-1xl text-white" />
-              </span>
-            </motion.button>
-          </motion.div>
+            <p>Connect with us</p>
+            <span className="rounded-full border-2 py-2 px-2">
+              <FaPeopleArrows className="text-1xl text-white" />
+            </span>
+          </motion.button>
         </div>
       </div>
     </section>
