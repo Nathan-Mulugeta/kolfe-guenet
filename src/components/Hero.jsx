fd05adb498e3a092f846210c4466d771eca59113
nathanMulugeta01 <nathanmulugetacoding@gmail.com>
2023-03-02 18:02:08 +0300

    added framer motion animation to svg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,43 +1,73 @@
 import { FaPeopleArrows } from "react-icons/fa";
 import { motion } from "framer-motion";
 
+const leftVariants = {
+  hidden: {
+    opacity: 0,
+    x: "-100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
+const rightVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
 function Hero() {
   return (
     <section id="hero">
       <div className="bg-secondary/50">
         <div className="container mx-auto flex h-screen flex-col items-center justify-center space-y-8 md:space-y-16 ">
-          <div className="flex flex-col items-center space-y-6 px-2 text-center md:px-0">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            className="flex flex-col items-center space-y-6 px-2 text-center md:px-0"
+          >
             <motion.h1
               className="font-glock text-6xl font-bold text-white md:text-8xl"
-              initial={{ opacity: 0, x: "-100vw" }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.5 }}
+              variants={leftVariants}
             >
               Welcome
             </motion.h1>
             <motion.p
               className="text-xl text-white md:text-2xl"
-              initial={{ opacity: 0, x: "100vw" }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 1, type: "spring", delay: 0.5 }}
+              variants={rightVariants}
             >
               Getting started with Kolfe Guenet church.
             </motion.p>
-          </div>
-
-          <motion.button
-            whileHover={{
-              scale: 1.1,
-              boxShadow: "0 4px 3px rgb(0 0 0 / 0.07)",
-            }}
-            whileTap={{ scale: 1 }}
-            className="flex items-center gap-3 rounded-md bg-secondary/75 py-2 px-6 font-bold text-white transition-all duration-200 ease-in-out"
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, type: "spring", delay: 0.5 }}
           >
-            <p>Connect with us</p>
-            <span className="rounded-full border-2 py-2 px-2">
-              <FaPeopleArrows className="text-1xl text-white" />
-            </span>
-          </motion.button>
+            <button className="flex items-center gap-3 rounded-md bg-secondary/75 py-2 px-6 font-bold text-white transition-all duration-200 ease-in-out hover:scale-110 hover:shadow-md active:scale-100">
+              <p>Connect with us</p>
+              <span className="rounded-full border-2 py-2 px-2">
+                <FaPeopleArrows className="text-1xl text-white" />
+              </span>
+            </button>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { useState, useRef, useEffect } from "react";
 import { NavLink, Link } from "react-router-dom";
-import churchLogo from "../assets/jpg/logo.png";
+// import churchLogo from "../assets/jpg/logo.png";
+// import churchLogoSvg from "../assets/svg/churchLogoSvg.svg";
 import { RxAvatar } from "react-icons/rx";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   const [navOpen, setNavOpen] = useState(false);
@@ -9,6 +11,34 @@ export default function Navbar() {
   const profileRef = useRef(null);
   const navRef = useRef(null);
   const [scroll, setScroll] = useState(false);
+
+  const svgVariant = {
+    hidden: {
+      scale: 100,
+    },
+    visible: {
+      scale: [100, 0.5, 1],
+      transition: {
+        duration: 3,
+        ease: "easeInOut",
+        type: "spring",
+        stiffness: 300,
+      },
+    },
+  };
+
+  const pathVariant = {
+    hidden: {
+      pathLength: 0,
+    },
+    visible: {
+      pathLength: 1,
+      transition: {
+        duration: 3,
+        ease: "easeInOut",
+      },
+    },
+  };
 
   useEffect(() => {
     document.addEventListener("click", handleProfileClickOutside);
@@ -71,7 +101,15 @@ export default function Navbar() {
 
         <div className="container mx-auto flex items-center justify-between px-4">
           {/* Nav bar hamburger icon */}
-          <div className={`${scroll ? "" : "rounded-md bg-secondary/50"} px-1`}>
+          <motion.div
+            whileHover={{
+              scale: 1.1,
+            }}
+            whileTap={{
+              scale: 0.9,
+            }}
+            className={`${scroll ? "" : "rounded-md bg-secondary/50"} px-1`}
+          >
             <button
               onClick={handleNavClick}
               id="menu-btn"
@@ -93,15 +131,67 @@ export default function Navbar() {
                 } hamburger-bottom`}
               ></span>
             </button>
-          </div>
+          </motion.div>
 
           {/* Church Logo */}
           <NavLink to="/">
-            <img
-              className={`h-8 w-auto rounded-md bg-white`}
-              src={churchLogo}
+            <motion.svg
+              className={`w-auto rounded-md`}
+              variants={svgVariant}
+              initial="hidden"
+              animate="visible"
+              width="40"
+              height="40"
+              viewBox="0 0 562 562"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <motion.path
+                variants={pathVariant}
+                d="M141 321.5H416M141 321.5L115.5 429H436.5L416 321.5M141 321.5L121 332.5L88 455H320M416 321.5H436.5L465.5 455H358M320 455V500C327.667 491.5 346 479.6 358 500V455M320 455H358"
+                stroke="#D1233A"
+                strokeWidth="10"
+              />
+              <motion.path
+                variants={pathVariant}
+                d="M410.5 136H126V174.5H410.5V136Z"
+                stroke="#D42037"
+                strokeWidth="10"
+              />
+              <motion.path
+                variants={pathVariant}
+                d="M243.5 511.5V41H297.5V511.5H243.5Z"
+                stroke="#D1233A"
+                strokeWidth="10"
+              />
+              <motion.circle
+                variants={pathVariant}
+                cx="281"
+                cy="281"
+                r="275"
+                stroke="#55A6D9"
+                strokeWidth="12"
+              />
+              <motion.path
+                variants={pathVariant}
+                d="M178 55.5439C178 55.5439 163 60.5877 163 66.5439C163 72.5 164 66.5439 170.5 100.544C175.955 129.079 195.424 168.544 205.5 184.544C205.5 184.544 230 217.044 245.5 226.044C293 241.044 310 236.544 312.5 236.544C326.5 232.544 324 232.544 330 232.544C356.5 230.044 366 252.544 366 250.044C371.2 230.444 367.167 215.877 364.5 211.044C360.5 204.544 351 204.344 349.5 203.044C342 196.544 342.5 196.044 340 190.544C338 186.144 342.5 180.044 345 177.544C370 147.544 375.325 109.71 378 99.5439C380.5 90.0439 381 23.0439 378 25.0439C375 27.0439 341 100.044 320 124.044C299 148.044 278 149.044 274 149.044C236.8 151.444 194.5 87.7105 178 55.5439Z"
+                stroke="#55A6D9"
+                strokeWidth="10"
+              />
+              <motion.circle
+                variants={pathVariant}
+                cx="350.5"
+                cy="219.5"
+                r="5.5"
+                fill="#55A6D9"
+              />
+            </motion.svg>
+
+            {/* <img
+              className={`h-8 w-auto rounded-md`}
+              src={churchLogoSvg}
               alt="Kolfe Guenet Church"
-            />
+            /> */}
           </NavLink>
 
           {/* Menu items */}
@@ -205,52 +295,84 @@ export default function Navbar() {
               to="/"
               className={({ isActive }) =>
                 `${
-                  isActive
-                    ? "bg-secondary text-white"
-                    : "hover:text-secondary/50"
+                  isActive ? "bg-secondary text-white" : ""
                 } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
-              Home
+              <motion.button
+                whileHover={{
+                  scale: 1.3,
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                }}
+              >
+                Home
+              </motion.button>
             </NavLink>
             <NavLink
               onClick={handleNavClick}
               to="/B"
               className={({ isActive }) =>
                 `${
-                  isActive
-                    ? "bg-secondary text-white"
-                    : "hover:text-secondary/50"
+                  isActive ? "bg-secondary text-white" : ""
                 } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
-              Team
+              <motion.button
+                whileHover={{
+                  scale: 1.3,
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                }}
+              >
+                Team
+              </motion.button>
             </NavLink>
             <NavLink
               onClick={handleNavClick}
               to="/C"
               className={({ isActive }) =>
                 `${
-                  isActive
-                    ? "bg-secondary text-white"
-                    : "hover:text-secondary/50"
+                  isActive ? "bg-secondary text-white" : ""
                 } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
-              Calendar
+              <motion.button
+                whileHover={{
+                  scale: 1.3,
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                }}
+              >
+                Calendar
+              </motion.button>
             </NavLink>
             <NavLink
               onClick={handleNavClick}
               to="/D"
               className={({ isActive }) =>
                 `${
-                  isActive
-                    ? "bg-secondary text-white"
-                    : "hover:text-secondary/50"
+                  isActive ? "bg-secondary text-white" : ""
                 } rounded-md px-3 py-1 text-secondary transition duration-200 ease-in-out`
               }
             >
-              Price
+              <motion.button
+                whileHover={{
+                  scale: 1.3,
+                }}
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                }}
+              >
+                Price
+              </motion.button>
             </NavLink>
           </ul>
         </div>
