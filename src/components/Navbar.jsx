68460b20ca78a219cae0347638fda7e246a8fe99
nathanMulugeta01 <nathanmulugetacoding@gmail.com>
2023-03-11 19:13:50 +0300

    made the sign in button look good

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -360,11 +360,40 @@ export default function Navbar() {
 
           <button
             onClick={handleSignInClick}
-            className={`${scroll ? "text-secondary" : "text-white"} ${
-              loading ? "bg-white/80" : ""
-            } rounded-full p-1 text-sm hover:text-black`}
+            className={`rounded-full px-4 py-2 text-sm font-semibold text-white ${
+              scroll ? "text-secondary" : ""
+            } ${
+              loading
+                ? "cursor-wait bg-white/80"
+                : "bg-gradient-to-r from-primary to-secondary transition-all duration-300 ease-in-out hover:from-secondary hover:to-primary"
+            }`}
+            disabled={loading}
           >
-            {signedIn ? "Sign out" : "Sign in"}
+            {signedIn ? "Sign Out" : "Sign In"}
+            {loading && (
+              <span className="ml-2 animate-spin">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-4 w-4 animate-spin"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-3.647z"
+                  ></path>
+                </svg>
+              </span>
+            )}
           </button>
         </div>
 
